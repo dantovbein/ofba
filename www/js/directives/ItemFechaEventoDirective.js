912d80171e93ba3989d2ac9f2cea10c6671eb951
Dan Tovbein <dtovbein@gmail.com>
2015-12-28 08:44:14 -0300

    Trabajando sobre el tiempo en epoch

--- www/js/directives/ItemFechaEventoDirective.js
+++ www/js/directives/ItemFechaEventoDirective.js
@@ -26,10 +26,31 @@ angular
 					$scope.isConfirmed = $scope.addFecha($scope.parseFecha($scope.fecha));
 				}
 
-				$scope.parseFecha = function(date) {
-					var time = Date.parse(date).toString()
-					var epochTime = time.slice(0,time.length-3);
-					return epochTime;
+				$scope.parseFecha = function(fecha) {
+					//console.log(fecha);
+					//var time = Date.parse(fecha).toString();
+					return Date.parse(fecha).toString();
+					//var tmpDate = new Date();
+					//tmpDate.setTime(time);
+
+					//var epochTime = new Date(tmpDate.getFullYear(),tmpDate.getMonth(),tmpDate.getDate(),tmpDate.getHours(),tmpDate.getMinutes(),tmpDate.getSeconds(),tmpDate.getMilliseconds()) / 1000;
+					//var epochTime = new Date(tmpDate.getFullYear(),tmpDate.getMonth(),tmpDate.getDate(),tmpDate.getHours(),tmpDate.getMinutes(),tmpDate.getSeconds()) / 1000;
+					
+					//return epochTime;
+					//console.log(time);
+					//return Math.round(tmpDate.getTime()/1000.0);
+					//return Math.round(new Date().getTime()/1000.0)
+					//var parts = time.match(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/);
+					//return Date.UTC(+parts[3], parts[2]-1, +parts[1], +parts[4], +parts[5]);
+					//var epochTime = time.slice(0,time.length-3);
+					//return epochTime;
+					//debugger;
+					//return time;
+
+					//var dateVal ="/Date("+time+")/";
+					//return new Date( parseFloat( dateVal.substr(6)));
+					//console.log(Date.now());
+					//return Math.floor(Date.now() / 1000);
 				}
 			}
 		}
